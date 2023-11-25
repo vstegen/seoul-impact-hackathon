dc1483f61717186d19c2be97adb02394207587d6
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-11-25 22:18:44 +0900

    Add CORS header allowlisting

--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,9 @@ module youth-korea
 go 1.21.1
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.11.3 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -13,4 +16,5 @@ require (
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"youth-korea/food"
 	"youth-korea/shelters"
@@ -9,9 +10,16 @@ import (
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowOrigin},
+	}))
+
 	e.GET("/shelters", shelters.GetShelters)
 	e.GET("/shelters/:id", shelters.GetShelter)
 	e.GET("/food", food.GetFacilities)
 	e.GET("/food/:id", food.GetFacility)
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
