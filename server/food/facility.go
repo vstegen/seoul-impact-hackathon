7ebb8a1203600a4bdb16c38b2f6205ce96452dc6
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2023-11-25 17:46:44 +0900

    Fix tag definition

--- server/food/facility.go
+++ server/food/facility.go
@@ -11,11 +11,11 @@ type Facility struct {
 	Id            int              `json:"id"`
 	OpeningTime   string           `json:"openingTime"`
 	Rating        *int             `json:"rating,omitempty"`
-	Desc          *string          `json:"desc",omitempty"`
+	Desc          *string          `json:"desc,omitempty"`
 	Address       string           `json:"address"`
 	Name          string           `json:"name"`
 	CurrentStatus RestaurantStatus `json:"currentStatus"`
-	Announcement  *string          `json:"announcement",omitempty"`
+	Announcement  *string          `json:"announcement,omitempty"`
 	Contact       *string          `json:"contact,omitempty"`
 	Website       *string          `json:"website,omitempty"`
 }
